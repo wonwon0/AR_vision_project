caea192bd4b656503f4e7b0fbecaa8ef41327e63
philippe.lebel <philippe.lebel008@sympatico.ca>
2022-12-05 14:33:26 -0500

    debut du travail sur viz

---
+++ 3d_object_test.py
@@ -0,0 +1,5 @@
+import cv2
+
+# mesh = cv2.viz.readMesh('./3d_objects/stanford_bunny.ply')
+cv2.viz.Viz3d_create("Viz Demo")
+# cv2.viz
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import vimba
 import cv2
 import numpy as np
+from calibration_routine import calibrate_camera
+from cv2 import aruco
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -10,44 +12,54 @@ objp[:,:2] = np.mgrid[0:8,0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
+mtx = np.loadtxt('mtx.txt', dtype=float)
+dist = np.loadtxt('dist.txt', dtype=float)
+
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
+board = aruco.CharucoBoard_create(7, 5, 0.03, 0.015, aruco_dict)
+imboard = board.draw((1000, 1000))
+cv2.imwrite('Charuco_board.png', imboard)
+cv2.namedWindow("charuco")
+cv2.imshow("charuco", imboard)
+arucoParams = aruco.DetectorParameters_create()
+last_img = None
+rvec=np.array([])
+tvec=np.array([])
 with vimba.Vimba.get_instance() as vmb:
     cams = vmb.get_all_cameras()
     with cams[0] as cam:
         # Adjust to the Bayer format you want to use
         cam.set_pixel_format(vimba.PixelFormat.BayerRG8)
+        cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
         while 1:
             # Record single frame for this example
             frame = cam.get_frame()
             # Get the raw image data as numpy array from the frame
             image = frame.as_numpy_ndarray()
             # Use opencv to convert raw Bayer image to RGB image
-            img  = cv2.cvtColor(image, cv2.COLOR_BAYER_RG2RGB)
-            cv2.imwrite('uncalibrated.png', img)
-            img2 = img
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # Find the chess board corners
-            ret, corners = cv2.findChessboardCorners(gray, (8, 5), flags=None)
-            # If found, add object points, image points (after refining them)
-            if ret == True:
-                objpoints.append(objp)
-                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-                imgpoints.append(corners2)
-                # Draw and display the corners
-                cv2.drawChessboardCorners(img, (8, 5), corners2, ret)
-                cv2.imshow('img', img)
-                cv2.waitKey(0)
-                ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+            img = cv2.cvtColor(image, cv2.COLOR_BAYER_RG2RGB)
+            #dst = cv2.undistort(img, mtx, dist, None)
+            dst=img
+            dst_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+            if last_img is not img:
+                corners, ids, rejectedImgPoints = aruco.detectMarkers(dst, aruco_dict,
+                                                                      parameters=arucoParams)
+                aruco.refineDetectedMarkers(dst_gray, board, corners, ids, rejectedImgPoints)
+                if len(corners) > 0:
+                    charucoretval, charucoCorners, charucoIds = aruco.interpolateCornersCharuco(corners, ids,
+                                                                                                dst_gray,
+                                                                                                board)
+                    im_with_charuco_board = aruco.drawDetectedCornersCharuco(dst, charucoCorners, charucoIds,
+                                                                             (0, 255, 0))
+                    retval, rvec, tvec = aruco.estimatePoseCharucoBoard(charucoCorners, charucoIds, board, mtx, dist, rvec, tvec)  # posture estimation from a charuco board
+                    if retval == True:
+                        dst = cv2.drawFrameAxes(dst, mtx, dist, rvec, tvec, 0.1)  # axis length 100 can be changed according to your requirement
+                        print(tvec)
 
-                h, w = img2.shape[:2]
-                newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-
-                # undistort
-                dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-
-                # crop the image
-                # x, y, w, h = roi
-                # dst = dst[y:y + h, x:x + w]
-                cv2.imwrite('calibresult.png', dst)
+                cv2.imshow("output", dst)
+                # cv2.imshow("output_uncal", img)
+                cv2.waitKey(100)
 
 
 cv2.destroyAllWindows()
